52914efc4628b3ea8b30d2a2014c49d9620cce3a
Tari Bence <bence.tari@gmail.com>
2022-01-25 16:35:21 +0100

    Use our own version system

--- config/version.mk
+++ config/version.mk
@@ -1,5 +1,6 @@
 # Copyright (C) 2016 The Pure Nexus Project
 # Copyright (C) 2016 The JDCTeam
+# Copyright (C) 2021 CrystalOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,58 +14,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-CRYSTAL_MOD_VERSION = v12.1
-CRYSTAL_BUILD_TYPE := UNOFFICIAL
-CRYSTAL_BUILD_ZIP_TYPE := VANILLA
+CRYSTAL_VERSION=1.0
+CRYSTAL_REGULAR_VERSION_NAME=Amethyst
+CRYSTAL_CLEAR_VERSION_NAME=Amber
+CRYSTAL_BUILD_TYPE?=ARGENT
+CRYSTAL_MAINTAINER?=NOBODY
+CRYSTAL_PURITY?=VANILLA
+CRYSTAL_CUSTOM_TYPE?=REGULAR
+CRYSTAL_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
 
-ifeq ($(CRYSTAL_BETA),true)
-    CRYSTAL_BUILD_TYPE := BETA
+CRYSTAL_DATE_YEAR := $(shell date -u +%Y)
+CRYSTAL_DATE_MONTH := $(shell date -u +%m)
+CRYSTAL_DATE_DAY := $(shell date -u +%d)
+
+CRYSTAL_BUILD_DATE := $(CRYSTAL_DATE_YEAR).$(CRYSTAL_DATE_MONTH).$(CRYSTAL_DATE_DAY)
+
+ifeq ($(CRYSTAL_CUSTOM_TYPE),CRYSTALCLEAR)
+    CRYSTAL_DISPLAY_VERSION=$(CRYSTAL_VERSION)-$(CRYSTAL_CLEAR_VERSION_NAME)
+    CRYSTAL_VERSION_NAME=$(CRYSTAL_CLEAR_VERSION_NAME)
+else
+    CRYSTAL_DISPLAY_VERSION=$(CRYSTAL_VERSION)-$(CRYSTAL_REGULAR_VERSION_NAME)
+    CRYSTAL_VERSION_NAME=$(CRYSTAL_REGULAR_VERSION_NAME)
 endif
-
-ifeq ($(CRYSTAL_GAPPS), true)
-    $(call inherit-product, vendor/gapps/common/common-vendor.mk)
-    CRYSTAL_BUILD_ZIP_TYPE := GAPPS
-endif
-
-CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
-
-ifeq ($(CRYSTAL_OFFICIAL), true)
-   LIST = $(shell cat infrastructure/devices/crystal.devices | awk '$$1 != "#" { print $$2 }')
-    ifeq ($(filter $(CURRENT_DEVICE), $(LIST)), $(CURRENT_DEVICE))
-      IS_OFFICIAL=true
-      CRYSTAL_BUILD_TYPE := OFFICIAL
-
-PRODUCT_PACKAGES += \
-    Updater
-
-    endif
-    ifneq ($(IS_OFFICIAL), true)
-       CRYSTAL_BUILD_TYPE := UNOFFICIAL
-       $(error Device is not official "$(CURRENT_DEVICE)")
-    endif
-endif
-
-ifeq ($(CRYSTAL_COMMUNITY), true)
-   LIST = $(shell cat infrastructure/devices/crystal-community.devices | awk '$$1 != "#" { print $$2 }')
-    ifeq ($(filter $(CURRENT_DEVICE), $(LIST)), $(CURRENT_DEVICE))
-      IS_COMMUNITY=true
-      CRYSTAL_BUILD_TYPE := COMMUNITY
-    endif
-    ifneq ($(IS_COMMUNITY), true)
-       CRYSTAL_BUILD_TYPE := UNOFFICIAL
-       $(error This isn't a community device "$(CURRENT_DEVICE)")
-    endif
-endif
-
-CRYSTAL_VERSION := Crystal-$(CRYSTAL_MOD_VERSION)-$(CURRENT_DEVICE)-$(CRYSTAL_BUILD_TYPE)-$(shell date -u +%Y%m%d)-$(CRYSTAL_BUILD_ZIP_TYPE)
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-  ro.crystal.version=$(CRYSTAL_VERSION) \
-  ro.crystal.releasetype=$(CRYSTAL_BUILD_TYPE) \
-  ro.crystal.ziptype=$(CRYSTAL_BUILD_ZIP_TYPE) \
-  ro.modversion=$(CRYSTAL_MOD_VERSION)
-
-CRYSTAL_DISPLAY_VERSION := Crystal-$(CRYSTAL_MOD_VERSION)-$(CRYSTAL_BUILD_TYPE)
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-  ro.crystal.display.version=$(CRYSTAL_DISPLAY_VERSION)
+    ro.crystal.version=$(CRYSTAL_VERSION) \
+    ro.crystal.buildtype=$(CRYSTAL_BUILD_TYPE) \
+    ro.crystal.maintainer=$(CRYSTAL_MAINTAINER) \
+    ro.crystal.purity=$(CRYSTAL_PURITY) \
+    ro.crystal.custom.type=$(CRYSTAL_CUSTOM_TYPE) \
+    ro.crystal.display.version=$(CRYSTAL_DISPLAY_VERSION) \
+    ro.crystal.build.date=$(CRYSTAL_BUILD_DATE)
