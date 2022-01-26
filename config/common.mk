151e7523a0bcd5908048ff7b216551487ab1287f
Tari Bence <bence.tari@gmail.com>
2022-01-26 17:40:14 +0100

    Use Arrow Gapps for now for Gapps buildtype

--- config/common.mk
+++ config/common.mk
@@ -98,6 +98,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+ifeq ($(CRYSTAL_PURITY),GAPPS)
+    $(call inherit-product, vendor/gapps/common/common-vendor.mk)
+endif
+
 # Bootanimation
 include vendor/crystal/config/bootanimation.mk
 
