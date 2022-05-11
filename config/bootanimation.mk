bdd66f862e5c8dffe465835705664cb04fef8afa
Tari Bence <bence.tari@gmail.com>
2022-05-11 08:53:38 +0200

    Change default Bootanimation resolution to 1080p and warn the user about it

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -9,7 +9,12 @@ PRODUCT_COPY_FILES += vendor/crystal/prebuilt/common/bootanimation/720.zip:$(TAR
 # 480p
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),480)
 PRODUCT_COPY_FILES += vendor/crystal/prebuilt/common/bootanimation/480.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-# Default to 720p if the device does not set the flag.
+# Default to 1080p and warn the user if the device does not set the flag.
 else
-PRODUCT_COPY_FILES += vendor/crystal/prebuilt/common/bootanimation/720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "CrystalOS: TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    else
+        $(warning "CrystalOS: Current bootanimation res is not supported, forcing 1080p")
+    endif
+    PRODUCT_COPY_FILES += vendor/crystal/prebuilt/common/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 endif
